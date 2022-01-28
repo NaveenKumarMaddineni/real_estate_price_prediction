083d9ac6927319e0e4b7652933e71b02cb9029a5
Naveen Kumar Maddineni <45824363+NaveenKumarMaddineni@users.noreply.github.com>
2022-01-29 03:18:58 +0530

    changed file structure to host on aws

--- server.py
+++ server.py
@@ -4,12 +4,12 @@ import util
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return render_template("../client/app.html")
+@app.route("/")
+def home_page():
+    return render_template("app.html")
 
 
-@app.route('/get_location_names')
+@app.route('/get_location_names', methods=['GET'])
 def get_location_names():
     response = jsonify({
         'locations': util.get_location_names()
@@ -19,23 +19,22 @@ def get_location_names():
     return response
 
 
-@app.route('/predict_home_price', methods=['POST'])
+@app.route('/predict_home_price', methods=['GET', 'POST'])
 def predict_home_price():
     total_sqft = float(request.form['total_sqft'])
     location = request.form['location']
-    bath = int(request.form['bath'])
     bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
 
     response = jsonify({
         'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
     })
-
     response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
 
 
-if __name__ == '__main__':
-    print("Startin Python Flask Server for Home Price Prediction...")
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
     util.load_saved_artifacts()
-    app.run(host='0.0.0.0', port=8080)
+    app.run(debug=True, host="0.0.0.0", port=80)
