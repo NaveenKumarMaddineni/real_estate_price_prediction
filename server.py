6fb0e546ea9c00b5074e688969b2ac460569f2fb
Naveen Kumar Maddineni <45824363+NaveenKumarMaddineni@users.noreply.github.com>
2022-01-29 04:24:49 +0530

    Update server.py

--- server.py
+++ server.py
@@ -37,4 +37,4 @@ def predict_home_price():
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
     util.load_saved_artifacts()
-    app.run(debug=True, host="0.0.0.0", port=8888)
+    app.run(debug=True, host="0.0.0.0", port=80)
